e67d68bc243660a84ffddb3a09d7553973c37a2b
Rama Agung Supriyadi <67418383+Bl4ck-lion@users.noreply.github.com>
2025-11-25 23:21:27 +0700

    Create [roomId].js

---
+++ pages/study-room/[roomId].js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+import StudyRoom from '../../components/StudyRoom'
+
+export default function StudyRoomPage() {
+  return (
+    <Layout>
+      <StudyRoom />
+    </Layout>
+  )
+}
